c77cb9d7fc1840c32d1b9a347236ddfdffccf844
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-07-18 22:35:34 +0400

    Refine comments in log_stats.py for better clarity
    Updated comments for clarity and corrected phrasing.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -10,9 +10,7 @@ def log_stats():
     Nginx kolleksiyasındakı sənədlərin sayını, metodları və
     status check sayını hesablayıb çap edir.
     """
-    # MongoDB bağlantısını qururuq
     client = MongoClient('mongodb://127.0.0.1:27017')
-    # logs verilənlər bazasının nginx kolleksiyasını seçirik
     nginx_collection = client.logs.nginx
 
     # 1. Ümumi loqların sayı
@@ -26,7 +24,7 @@ def log_stats():
         count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    # 3. Status check sayı (həm GET metodu, həm də /status path-i olmalıdır)
+    # 3. Status check sayı (həm GET metodu, həm də /status path-i bir yerdə)
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
