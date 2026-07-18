27edbc0669bea84abad56d062782416911e7e345
Aitaj Gahraman <117116446+aitajghrmn@users.noreply.github.com>
2026-07-18 22:36:18 +0400

    Update log_stats function for better stats reporting
    Refactor log_stats function to accept parameters and improve functionality.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python3
-"""
-MongoDB-də saxlanılan Nginx loqları haqqında statistika təqdim edən skript.
+"""log stats from collection
 """
 from pymongo import MongoClient
 
 
-def log_stats():
-    """
-    Nginx kolleksiyasındakı sənədlərin sayını, metodları və
-    status check sayını hesablayıb çap edir.
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    # 1. Ümumi loqların sayı
-    total_logs = nginx_collection.count_documents({})
-    print(f"{total_logs} logs")
 
-    # 2. HTTP Metodlarının statistikası
+def log_stats(mongo_collection, option=None):
+    """ script that provides some stats about Nginx logs stored in MongoDB
+    """
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return
+
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
     print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    # 3. Status check sayı (həm GET metodu, həm də /status path-i bir yerdə)
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
-    log_stats()
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
